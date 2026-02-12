1d1d56644392c59c638b94f893878b3dddbfcbea
Sreehari S <sreeharisunil100@gmail.com>
2026-02-12 15:32:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 // Prevent scroll lag on mobile
 document.addEventListener("touchmove", function () {}, { passive: true });
+let selectedDiscipline = null;
+let selectedSemester = null;
+
+function selectDiscipline(btn, value) {
+  document.querySelectorAll('#disciplineGroup button')
+    .forEach(b => b.classList.remove('active'));
+
+  btn.classList.add('active');
+  selectedDiscipline = value;
+}
+
+function selectSemester(btn, value) {
+  document.querySelectorAll('#semesterGroup button')
+    .forEach(b => b.classList.remove('active'));
+
+  btn.classList.add('active');
+  selectedSemester = value;
+}
+
+function submitSelection() {
+  if (!selectedDiscipline || !selectedSemester) {
+    alert("Please select both discipline and semester.");
+    return;
+  }
+
+  window.location.href =
+    `results.html?course=bsc&discipline=${selectedDiscipline}&sem=${selectedSemester}`;
+}
